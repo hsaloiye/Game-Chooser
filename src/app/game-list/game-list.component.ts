a816b0c153fea00a5e97e28406d9b9bc4ed36af4
hsaloiye <hsaloiye205@gmail.com>
2019-11-16 10:44:49 -0500

    removing conflicting import

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {IGame} from "./game"
-import { unusedValueExportToPlacateAjd } from '@angular/core/src/render3/interfaces/container';
 import {GameListService} from './game-list.service';
 
 @Component({
